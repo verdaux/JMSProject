a1d1a509c68f36112d74447d452a7eba032dbc06
PB <xachineon@gmail.com>
2023-05-15 10:31:36 +0530

    Init

--- JMSProject/src/main/java/com/scorpz/JMSProject/FirstQueue.java
+++ JMSProject/src/main/java/com/scorpz/JMSProject/FirstQueue.java
@@ -12,7 +12,7 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 public class FirstQueue
-{
+{  
 	public static void main(String[] args)
 	{
 		InitialContext initialContext = null;
